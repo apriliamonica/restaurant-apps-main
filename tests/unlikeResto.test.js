006ee439f6866db04478bac39678748455631059
April <liapandeiroth34190@gmail.com>
2024-11-16 16:57:02 +0800

    Ketika Pikiranku Berubah

--- tests/unlikeResto.test.js
+++ tests/unlikeResto.test.js
@@ -2,12 +2,21 @@
 import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
 import FavoriteRestoIdb from '../src/public/data/favorite-resto-idb';
 
-describe('Liking A Resto', () => {
-  /* ...kode lainnya disembunyikan... */
-
-  it('should be able to like the resto', async () => {
+describe('Unliking A Resto', () => {
+  const addLikeButtonContainer = () => {
     document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
 
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestoIdb.putResto({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestoIdb.deleteResto(1);
+  });
+
+  it('should display unlike widget when the resto has been liked', async () => {
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       resto: {
@@ -15,12 +24,44 @@ describe('Liking A Resto', () => {
       },
     });
 
-    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
-
-    // Memastikan film berhasil disukai
-    const resto = await FavoriteRestoIdb.getAllResto(1);
-    expect(resto).toEqual[{ id: 1 }];
-
-    await FavoriteRestoIdb.deleteResto(1);
+    expect(document.querySelector('[aria-label="unlike this resto"]')).toBeTruthy();
   });
+
+  it('should not display like widget when the movie has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this resto"]')).toBeFalsy();
+  });
+
+
+  it('should be able to remove liked resto from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+    document.querySelector('[aria-label="unlike this resto"]').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestoIdb.getAllResto()).toEqual([]);
+  });
+
+  it('should not throw error when user click unlike widget if the unliked resto is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+    // Hapus dulu film dari daftar resto yang disukai
+    await FavoriteRestoIdb.deleteResto(1);
+    // Kemudian, simulasikan pengguna menekan widget batal menyukai resto
+    document.querySelector('[aria-label="unlike this resto"]').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestoIdb.getAllResto()).toEqual([]);
+  });
+
 });
