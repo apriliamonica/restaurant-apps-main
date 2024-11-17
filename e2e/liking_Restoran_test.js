55feda3aa5c75759687f526f251f117ba3657e58
April <liapandeiroth34190@gmail.com>
2024-11-17 11:09:16 +0800

    resto test 0

--- e2e/liking_Restoran_test.js
+++ e2e/liking_Restoran_test.js
@@ -8,11 +8,26 @@ Before(({ I }) => {
 
 Scenario('showing empty liked resto', ({ I }) => {
   I.seeElement('#query');
-  I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
+  I.see('', '.list');
 });
 
-Scenario('liking one resto', ({ I }) => {
+const assert =require('assert');
+
+Scenario('liking one resto', async ({ I }) => {
   I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
   I.amOnPage('/');
-  pause();
+
+  I.seeElement('.resto__title a');
+  const firstResto = locate('.resto__title a').first();
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.resto-item');
+  const likedRestoTitle = await I.grabTextFrom('.resto__title');
+
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
 });
